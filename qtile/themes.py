f4ac0b1426bae973d71758a550abd426feaadde2
researcher2312 <researcher2312@gmail.com>
2023-11-14 23:39:04 +0100

    new config file for spacemacs

--- qtile/themes.py
+++ qtile/themes.py
@@ -1,5 +1,6 @@
 import abc
 from widgets import SystemConfigurationValues
+import json
 
 class QtileTheme:
     def __init__(self, qtile, sysConfig):
@@ -9,13 +10,20 @@ class QtileTheme:
         self.font_name = None
         self.status_bar = None
         self.status_popup = None
-        self.colors = []
+        self.light_colors = {}
+        self.dark_colors = {}
         self.sysConfig = sysConfig
 
     def apply_theme(self):
-        #self.qtile_handle.screens[0].set_wallpaper(self.wallpaper_path)
+        self.sysConfig.wallpaper = self.wallpaper_path
         self.sysConfig.volume_popup = self.status_popup
         self.sysConfig.system_bar = self.status_bar
 
     def set_wallpaper(self, wallpaper):
         self.wallpaper_path = wallpaper
+
+    def load_colors(self, path):
+        file = open(path)
+        data = json.load(file)
+        self.light_colors = data["light"]
+        self.dark_colors = data["dark"]
